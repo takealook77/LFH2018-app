0c66ea3ab6f94e39d16edb236e9008ebb99cffa5
piantino <piantino@gmail.com>
2018-05-25 17:15:48 -0300

    Adicionado pesquisa

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ app.controller("ementaCtrl", function($scope) {
 	
 	$scope.acordaos = ACORDAOS;
 });
+
+
+app.filter('cnj', function() {
+   return function(val){
+	   if (val) {		   
+		   return val.replace(/^(\d{7})(\d{2})(\d{4})(\d)(\d{2})(\d{4})$/, '$1-$2.$3.$4.$5.$6');
+	   }
+	   return val;
+   };
+});
