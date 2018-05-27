430fb465e04aa505ece3bf2f293e72ac02dd65ee
Piantino <piantino@gmail.com>
2018-05-27 18:49:59 -0300

    Alterado para buscar acordãos do elastic

--- public/app.js
+++ public/app.js
@@ -1,9 +1,36 @@
+"use strict";
+
 var app = angular.module("ia3vpApp", ['ngSanitize']);
 
 
-app.controller("ementaCtrl", function($scope) {
+app.controller("ementaCtrl", function($scope, $http) {
 	
-	$scope.acordaos = ACORDAOS;
+	$scope.buscar = function(textoBusca) {
+		var query = '';
+		if (textoBusca) {
+			query = 'q=ementa:' + textoBusca
+		}
+		$http({
+			method : 'GET',
+			url : 'http://localhost:9200/ia3vp/acordao/_search/?' + query
+		}).then(function successCallback(response) {
+			$scope.acordaos = [];
+			$scope.filtro = {};
+			
+			var data = response.data;
+			$scope.filtro.total = data.hits.total
+
+			angular.forEach(data.hits.hits, function(value, key) {
+				// TODO: Ajustar campos no índice
+				this.push(value['_source']);
+			}, $scope.acordaos);
+
+		}, function errorCallback(response) {
+			$scope.response = response;
+		});
+	};
+	
+	$scope.buscar();
 });
 
 
