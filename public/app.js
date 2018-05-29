b7655127c9c26e5dd8e8690baf14b0e409dc9d40
Piantino <piantino@gmail.com>
2018-05-29 20:46:54 -0300

    Adicionado API temporária

--- public/app.js
+++ public/app.js
@@ -2,18 +2,29 @@
 
 var app = angular.module("ia3vpApp", ['ngSanitize']);
 
+var serverUrl
 
-app.controller("acordaoCtrl", function($scope, $http) {
+
+app.controller("acordaoCtrl", function($rootScope, $scope, $http) {
+	
+	$rootScope.apiUrl = 'http://35.196.200.63/ia3vp/';
 	
 	$scope.filtro = {};
 	$scope.acordaoAtual = null;
 	
-	$scope.buscar = function(textoBusca) {	
+	$scope.buscar = function(textoBusca) {
+		var query = textoBusca ? 'q=*:' + textoBusca : '';
+		
 		$http({
 			method : 'GET',
-			url : 'http://localhost:8080/'
+			url : $rootScope.apiUrl + 'acordao/_search/?' + query
 		}).then(function successCallback(response) {
-			$scope.acordaos = response.data;
+			$scope.acordaos = [];
+			
+			var data = response.data;
+			angular.forEach(data.hits.hits, function(value, key) {
+				this.push(value['_source']);
+			}, $scope.acordaos);
 		}, function errorCallback(response) {
 			$scope.response = response;
 		});
